f5460095cc59f135bdd05cb120e2cbf0532d3d5c
hugo riquelme <hugo riquelme>
2017-06-26 03:41:40 -0400

    se agrega template y contenido de entrada con django

--- web/views.py
+++ web/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from .models import Entrada
+from django.views.generic import ListView
 # Create your views here.
+
+class IndexView (ListView):
+
+	template_name = 'index.html'
+	model = Entrada
